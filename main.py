a2fedefa8a564d4087b8cd3db96f98c9a3ab4fca
GIHOON LEE <gihoonlee@GIHOONui-MacBookPro.local>
2023-01-03 23:21:36 +0900

    DB 연동 코드 작성 - 아키텍처는 아직

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 app = FastAPI()
 
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_SCHEMA = os.environ.get('DB_SCHEMA')
+DB_PORT = os.environ.get('DB_PORT')
 
+DB_URL = 'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_SCHEMA}'
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    mySecret = os.environ.get('MySecret')
+    return {"Hello": "123"}
 
 
 @app.get("/items/{item_id}")
